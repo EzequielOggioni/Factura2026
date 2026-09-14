04386f81b1a20ad530f93c449bfccc27421e317a
Ezequiel Oggioni <ezequieloggioni@gmail.com>
2026-09-14 20:38:58 -0300

    generate csv

--- src/app/componentes/foro/foro.ts
+++ src/app/componentes/foro/foro.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore, query, orderBy, Unsubscribe, onSnapshot, deleteDoc, getDoc, where, getDocs, doc, DocumentReference, updateDoc } from "firebase/firestore";
@@ -10,11 +10,14 @@ import firebaseConfig from '../../JSON/firebaseConfig.json';
   imports: [CommonModule, FormsModule],
   templateUrl: './foro.html',
   styleUrl: './foro.scss',
+  providers: [DatePipe]
 })
 export class Foro implements OnInit, OnDestroy {
 
   public app = initializeApp(firebaseConfig);
   public db = getFirestore(this.app);
+
+  public datepipe:any  = inject(DatePipe);
 
   public mensajeForo: mensajeForo = {
     Categoria: '',
@@ -25,7 +28,7 @@ export class Foro implements OnInit, OnDestroy {
 
   private docRefModifica: DocumentReference | null = null;
   public modificando: WritableSignal<boolean> = signal(false);
-  
+
   public mensajesForo: WritableSignal<mensajeForo[]> = signal<mensajeForo[]>([]);
 
   private unsubscribe: Unsubscribe | null = null;
@@ -33,7 +36,7 @@ export class Foro implements OnInit, OnDestroy {
   ngOnInit(): void {
 
     this.unsubscribe = onSnapshot(query(collection(this.db, 'MensajesForo'),
-      where('Fecha', '>=', new Date(new Date().setDate(new Date().getDate() - 90))), 
+      where('Fecha', '>=', new Date(new Date().setDate(new Date().getDate() - 90))),
       orderBy('Fecha', 'desc')), (datos) => {
         this.mensajesForo.set(datos.docs.map((docSnap: any) => {
           const data = docSnap.data() as any;
@@ -45,7 +48,7 @@ export class Foro implements OnInit, OnDestroy {
         }));
       });
   }
- 
+
   public async enviar() {
     if (this.mensajeForo) {
       try {
@@ -94,5 +97,31 @@ export class Foro implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.unsubscribe?.();
   }
+
+  public generateTxtFile(fileName: string, textContent: string): void {
+    // 1. Creamos un Blob con el texto y definimos su tipo como texto plano
+    const blob: Blob = new Blob([textContent], { type: "text/csv;charset=utf-8" });
+
+    // 2. Creamos un elemento <a> temporal en la memoria del navegador
+    const link: HTMLAnchorElement = document.createElement("a");
+
+    // 3. Creamos una URL única que apunta a los datos de nuestro Blob
+    link.href = URL.createObjectURL(blob);
+
+    // 4. Asignamos el nombre por defecto que tendrá el archivo descargado
+    link.download = fileName;
+
+    // 5. Ocultamos el enlace, lo metemos en la página y simulamos el clic de descarga
+    link.style.display = "none";
+    document.body.appendChild(link);
+    link.click();
+
+    // 6. Limpiamos la memoria borrando el enlace y liberando la URL creada
+    document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
+
+    
+  }
+
 
 }
